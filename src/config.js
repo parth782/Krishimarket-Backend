f60eaec13bf593c07bbd5fdea5276205bcc9c14b
parth782 <parth11gupta@gmail.com>
2023-04-14 22:38:16 +0530

    bug fixing

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgres://uqjrychxytvvgs:ee3377e6ec9e0e35d530f629040d0b539eaaf334fa1db3ea296451c4d02fe38a@ec2-52-206-15-227.compute-1.amazonaws.com:5432/d2a1j1gfkjslgl',
-
     //'postgresql://postgres@localhost/farmers-bazaar',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     CLIENT_ORIGIN: '*',
